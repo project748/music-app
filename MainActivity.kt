0f01bc4462597ddd58f379f64e80439ebde5e475
project748 <cocvijeya@gmail.com>
2025-06-21 14:22:12 +0530

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,32 @@
+﻿import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private var isPlaying = false
+    private val songUrl = "https://www.dropbox.com/scl/fi/dgav25jp9t5r2jvnl868f/Deva-Deva-Brahmastra-128-Kbps.mp3?rlkey=gho5nolgq7t6cr0d2fg2vj068&raw=1"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val playButton = Button(this)
+        playButton.text = "Play"
+
+        playButton.setOnClickListener {
+            if (!isPlaying) {
+                val intent = Intent(this, MusicService::class.java)
+                intent.putExtra("songUrl", songUrl)
+                startService(intent)
+                playButton.text = "Pause"
+                isPlaying = true
+            } else {
+                stopService(Intent(this, MusicService::class.java))
+                playButton.text = "Play"
+                isPlaying = false
+            }
+        }
+
+        setContentView(playButton)
+    }
+}
---
+++ MusicService.kt
@@ -0,0 +1,27 @@
+﻿import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+
+class MusicService : Service() {
+    private lateinit var mediaPlayer: MediaPlayer
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val songUrl = intent?.getStringExtra("songUrl") ?: return START_NOT_STICKY
+        mediaPlayer = MediaPlayer()
+        mediaPlayer.setDataSource(songUrl)
+        mediaPlayer.prepare()
+        mediaPlayer.start()
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        mediaPlayer.stop()
+        mediaPlayer.release()
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+}
